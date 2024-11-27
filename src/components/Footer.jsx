b6c65d8f456f50366ea3caf57a5ae577864af115
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-11-27 13:24:17 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import "./Footer.css"
+import {Link} from "react-router-dom";
 const Footer =()=>{
     return(
         <div className={"footer"}>
             <p> &#169;Coffee shop</p>
             <p>{new Date().getFullYear()}</p>
-            <p>&lt;&lt;&lt; sli &gt;&gt;&gt;</p>
+            <p> <a href={"https://github.com/johnkinuthiaa/coffee_shop"}>&lt;&lt;&lt; sli &gt;&gt;&gt;</a> </p>
         </div>
     )
 }
